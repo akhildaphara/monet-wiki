65fb5370e154105e97bed9b45daae8684592f080
Akhil Daphara <akhildaphara@gmail.com>
2026-05-31 10:44:34 -0400

    add auth html

--- wiki-html/sidebar.js
+++ wiki-html/sidebar.js
@@ -49,6 +49,11 @@ document.addEventListener("DOMContentLoaded", () => {
             icon: `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"></path>`
         },
         {
+            href: "Auth.html",
+            label: "Authentication & Security",
+            icon: `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path>`
+        },
+        {
             href: "../design-system.html",
             label: "Monet Design System",
             icon: `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z"></path>`
